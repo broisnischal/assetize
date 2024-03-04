b2873e28be35e29cdac0d10a491e724dd3f7e52f
broisnischal <neeswebservice@gmail.com>
2024-03-04 23:09:56 +0545

    feat:transform-config

--- src/config/get-config.ts
+++ src/config/get-config.ts
@@ -2,65 +2,20 @@
 
 import * as zod from "zod";
 
+import _ from "lodash";
 import { cosmiconfig } from "cosmiconfig";
 import { logger } from "../logger";
 import { assetsDirectoryLike } from "./help-log";
-
-// const explorer = cosmiconfig("assetize");
-// console.log(explorer.search());
-
-// export async function loadUserConfig() {
-// const rootDir = process.cwd(); // Get the root directory of the user's project
-
-// console.log(rootDir);
-
-// const configPath = path.join(rootDir, "assetize.config.js");
-
-// console.log(configPath);
-
-// let config: AssetizeConfig;
-
-// try {
-//   // Dynamically import the user's configuration file
-//   const userConfigModule = await import(configPath);
-
-//   config = userConfigModule.default || userConfigModule; // Handle ES6 default exports
-// } catch (error) {
-//   console.error("Error loading user configuration:", error);
-//   config = {
-//     outputFile: "gen.ts",
-//     codebase: "remix",
-//   }; // Fallback to default configuration
-// }j
-
-// return config;
-
-//   console.log("testing");
-//   const explorer = cosmiconfig("assetize");
-//   console.log(explorer.load(process.cwd()));
-// }
-
-// async function main() {
-//   const userConfig = await loadUserConfig();
-//   console.log(userConfig);
-// }
-
-// main().catch(console.error);
-
-// let userConfig = import("../../assetize.config");
-
-// async function main() {
-//   const config = await userConfig;
-//   console.log(config);
-// }
-
-// main().catch(console.error);
 
 const configSchema = zod.object({
   output: zod.string().optional().default("./src"),
   outputFile: zod.string().optional().default("assetize.gen.ts"),
   lineLength: zod.number().optional().default(80),
-  className: zod.string().optional().default("MyAssets"),
+  className: zod
+    .string()
+    .optional()
+    .default("MyAssets")
+    .transform((x) => _.upperFirst(x).replace(/-/g, "")),
   case: zod
     .enum(["camel", "kebab", "pascal", "snake"])
     .optional()
@@ -129,26 +84,3 @@ export async function getConfig(): Promise<Config> {
 }
 
 export async function setConfig() {}
-
-// export async function loadConfig(): Promise<AssetizeConfig> {
-//   let configPath = "./assetize.config.ts";
-//   const explorer = cosmiconfig("assetize", {
-//     cache: false,
-//   });
-
-//   const explicitPath = configPath
-//     ? path.resolve(process.cwd(), configPath)
-//     : undefined;
-
-//   const explore = explicitPath ? explorer.load : explorer.search;
-//   const searchPath = explicitPath ? explicitPath : process.cwd();
-//   const local = await explore(searchPath);
-
-//   if (local) {
-//     return local.config satisfies AssetizeConfig;
-//   }
-
-//   return {
-//     codebase: "remix",
-//   } satisfies AssetizeConfig;
-// }
