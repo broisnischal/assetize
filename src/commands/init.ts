7459b544dcf114f693d6b1d10843bfa457ea9ffb
broisnischal <neeswebservice@gmail.com>
2024-03-08 12:12:41 +0545

    chore: configure the init.ts for starting

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,16 +1,12 @@
-// import { Command } from "commander";
 import chalk from "chalk";
-// import { configureLibraries } from "./library.js";
+import { Command } from "commander";
+import { logger } from "../logger";
 
-// export const init = new Command()
-//   .name("init")
-//   .description("initialize your project and choose libraries")
-//   .action(async () => {
-//     await configureLibraries();
+export const init = new Command()
+  .name("init")
+  .description("initialize your project and choose libraries")
+  .action(async () => {
+    // await configureLibraries();
 
-//     logger.info(`${chalk.green("Success!")} Project initialization completed.`);
-//   });
-
-export const init = () => {
-  console.log("init");
-};
+    logger.info(`${chalk.green("Success!")} Project initialization completed.`);
+  });
